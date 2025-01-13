25ffcdd5e94d582f8119fe93a307ee9ca8124a5a
YVakhnyak <namejfk@gmail.com>
2025-01-13 22:01:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter
-    // basename="/online-psy"
-    >
+    <BrowserRouter basename="/my-lending/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
