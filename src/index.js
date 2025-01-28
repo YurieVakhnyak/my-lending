c2330cad0c5dab5fadaa61a7c3eba2e3703014e1
YVakhnyak <namejfk@gmail.com>
2025-01-28 12:59:16 +0200

    Changed base route

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./assets/fonts/fonts.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-lending">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
